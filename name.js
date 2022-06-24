35d3828ebae518b22a9284cd8713b314a57e72c3
Lightskingod <Nwakasichinonso@gmail.com>
2022-06-24 14:56:17 +0100

    uploaded

---
+++ name.js
@@ -0,0 +1,9 @@
+const myName = 'Name: Hyacinth Nwakasi,'
+const height = ' Height: 6.1ft,'
+const country = ' Country: Nigeria.'
+
+window.alert(myName + height + country);
+
+document.write(myName);
+document.write(height);
+document.write(country);
